029d5628f8ce1a071dc9bef95602e1e9f5903af5
Andrew <andriy.lutsenko@flexmade.com>
2024-08-07 15:16:16 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,129 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable */
+import '@walletconnect/react-native-compat';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Pressable,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Core} from '@walletconnect/core';
+import {Web3Wallet} from '@walletconnect/web3wallet';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const core = new Core({
+  projectId: 'acbcd71cd6321fe035d4e23718352e01',
+});
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
+//eslint-ignore
+
+const App = ({}) => {
+  const [wallet, setWallet] = useState(null);
+  const [walletConnectURL, setWalletConnectURL] = useState(null);
+  const [sessionInfo, setSessionInfo] = useState(null);
+
+  const connectWallet = async () => {
+    console.log('Run connect...');
+    try {
+      const web3wallet = await Web3Wallet.init({
+        core,
+        metadata: {
+          name: 'ACM Wallet',
+          description: 'ACM wallet app',
+          url: 'www.walletconnect.com',
+          icons: ['https://your_wallet_icon.png'],
+          redirect: {
+            native: 'acm-wallet://',
           },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+        },
+      });
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+      web3wallet.on('session_proposal', async proposal => {
+        console.log('Session Proposal:', proposal);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        const requiredNamespaces = proposal.params.requiredNamespaces;
+        const namespaces = {};
+        // Создаем namespaces на основе требуемых namespaces
+        Object.keys(requiredNamespaces).forEach(key => {
+          namespaces[key] = {
+            methods: requiredNamespaces[key].methods,
+            chains: requiredNamespaces[key].chains,
+            events: requiredNamespaces[key].events,
+            accounts: requiredNamespaces[key].chains.map(
+              chain => `${chain}:${account}`,
+            ),
+          };
+        });
+
+        try {
+          const session = await web3wallet.approveSession({
+            id: proposal.id,
+            namespaces,
+          });
+
+          console.log('Session approved:', session);
+          setSessionInfo(session); // Сохраняем информацию о сессии
+        } catch (error) {
+          console.error('Error approving session:', error);
+        }
+      });
+
+      web3wallet.on('session_delete', () => {
+        console.log('Session deleted');
+        reset();
+      });
+
+      setWallet(web3wallet);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // await wallet.pair({ uri: code[0]?.value });
+
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
+  const connectToDApp = async () => {
+    console.log('Connect by URL Wallet Connect...');
+    try {
+      await wallet.pair({uri: walletConnectURL});
+      console.log('Success connect by URL Wallet Connect...');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const disconnect = async () => {
+    console.log('Disconnecting...');
+    try {
+      await wallet.disconnectSession({
+        topic: sessionInfo.topic,
+        reason: {
+          code: 1000,
+          message: 'User disconnected',
+        },
+      });
+      console.log('Disconnected successfully.');
+      reset();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const reset = () => {
+    setWalletConnectURL(null);
+    setSessionInfo(null); // Сбрасываем информацию о сессии при сбросе
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>Wallet Connect</Text>
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
